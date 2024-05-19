e4ed1231d5f523cfd4018eedeee66cbe2d0adec2
rodgerpeter <rodgerpeter20@gmail.com>
2024-05-18 22:58:55 -0700

    AirBnB clone - The console

---
+++ web_static/w3c-validator/w3c_validator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+W3C validator for Holberton School
+
+For HTML and CSS files.
+
+Based on 2 APIs:
+
+- https://validator.w3.org/nu/
+- http://jigsaw.w3.org/css-validator/validator
+
+
+Usage:
+
+Simple file:
+
+```
+./w3c_validator.py index.html
+```
